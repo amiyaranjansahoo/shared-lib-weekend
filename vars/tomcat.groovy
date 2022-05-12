2d188c1bcafad553135bd55263205d8a2ab362a9
amiyaranjansahoo <meamiyaranjan@gmail.com>
2022-05-12 14:53:12 +0530

    Create tomcat.groovy

---
+++ vars/tomcat.groovy
@@ -0,0 +1,7 @@
+def call(envt,user,ip) {
+  sshagent([envt]) {
+                        sh 'scp -o StrictHostKeyChecking=no target/myweb.war $user@$ip:/software/tomcat9/webapps/'
+                	      sh 'ssh $user@$ip /software/tomcat9/bin/shutdown.sh'
+                	      sh 'ssh $user@$ip /software/tomcat9/bin/startup.sh'
+                }
+}
