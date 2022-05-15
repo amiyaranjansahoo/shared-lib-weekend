529134197fc8a5dfdde074bd25f0445a5f47ba38
amiyaranjansahoo <meamiyaranjan@gmail.com>
2022-05-15 09:27:08 +0530

    Update deployTomcat.groovy

--- vars/deployTomcat.groovy
+++ vars/deployTomcat.groovy
@@ -1,8 +1,8 @@
 def call(envt,user_name,ip) {
-	sshagent([envt]) {
-        sh 'scp -o StrictHostKeyChecking=no target/myweb.war $user_name@$ip:/software/tomcat/webapps/'
-        sh 'ssh $user_name@$ip /software/tomcat/bin/shutdown.sh'
-        sh 'ssh $user_name@$ip /software/tomcat/bin/startup.sh'
+	sshagent(['envt']) {
+        sh "scp -o StrictHostKeyChecking=no target/myweb.war $user_name@$ip:/software/tomcat/webapps/"
+        sh "ssh $user_name@$ip /software/tomcat/bin/shutdown.sh"
+        sh "ssh $user_name@$ip /software/tomcat/bin/startup.sh"
     }
   
 }
